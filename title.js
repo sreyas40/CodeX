2f66deecba59a9ef6f4cf4ccd646b383602c89f3
Sreyas Kumar C V <sreyaskumar40@gmail.com>
2023-04-25 19:33:45 +0530

    ui rework

--- title.js
+++ title.js
@@ -1,6 +1,8 @@
 const title = document.querySelector(".club_title");
 
-setTimeout(() => { title.textContent = "COOEX" }, 1000);
-setTimeout(() => { title.textContent = "COD3X" }, 2000);
-setTimeout(() => { title.textContent ="CODE#" }, 3000);
-setTimeout(() => { title.textContent = "6ODEX"}, 4000);
+setTimeout(() => { title.textContent = "6003#" }, 1000);
+setTimeout(() => { title.textContent = "C003#" }, 2000);
+setTimeout(() => { title.textContent ="CO03#" }, 3000);
+setTimeout(() => { title.textContent = "COD3#" }, 4000);
+setTimeout(() => { title.textContent = "CODE#" }, 5000);
+setTimeout(() => { title.textContent = "CODEX" }, 6000);
