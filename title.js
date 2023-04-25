f4f90233132706252c09f28e014da58c9c870348
Sreyas Kumar C V <sreyaskumar40@gmail.com>
2023-04-25 19:28:00 +0530

    ui rework

---
+++ title.js
@@ -0,0 +1,6 @@
+const title = document.querySelector(".club_title");
+
+setTimeout(() => { title.textContent = "கோஸ்" }, 1000);
+setTimeout(() => { title.textContent = "कोडेक्स" }, 2000);
+setTimeout(() => { title.textContent ="കോഡെക്സ്" }, 3000);
+setTimeout(() => { title.textContent = "CODEX"}, 4000);
